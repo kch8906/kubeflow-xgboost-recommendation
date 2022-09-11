710d5d10293f3e33613a61cbf44aca03999ad845
kch8906 <kch8906@naver.com>
2022-09-12 00:28:55 +0900

    Error: Problem occurred when moving csv data on kubeflow

--- 01.download_from_s3/download.py
+++ 01.download_from_s3/download.py
@@ -3,16 +3,17 @@ import boto3
 
 def s3_download():
     s3_client = boto3.client('s3', region_name='ap-northeast-2',
-                         aws_access_key_id='AKIAWPQA2KXZ7LKY4PWI', 
-                         aws_secret_access_key='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')
+                         aws_access_key_id='aws_access_key_id', 
+                         aws_secret_access_key='aws_secret_access_key')
 
-    bucket = 'changhyun-kubeflow'
-    filename = ['LPOINT_BIG_COMP_01_DEMO.csv', 'LPOINT_BIG_COMP_02_PDDE.csv', 'LPOINT_BIG_COMP_05_BR.csv']
-    object_name = ['df_01.csv', 'df_02.csv', 'df_03.csv']
+    bucket = 'changhyun-xgboost'
+    filename = ['LPOINT_BIG_COMP_01_DEMO.csv', 'LPOINT_BIG_COMP_02_PDDE.csv', 'LPOINT_BIG_COMP_04_PD_CLAC.csv']
+    object_name = ['df_customer.csv', 'df_purchase.csv', 'df_product.csv']
 
     for i in range(len(filename)):
         s3_client.download_file(bucket, filename[i], object_name[i])
 
+    
     
 if __name__ == "__main__":
     print("Downloading data..")
--- kubeflow_pipeline.py
+++ kubeflow_pipeline.py
@@ -2,30 +2,49 @@ import kfp
 from kfp import dsl
 from kfp.components import create_component_from_func
 
+
 def downloadFile_op():
 
     return dsl.ContainerOp(
         name='Download Data',
-        image='crysiss/download_file:0.1',
+        image='crysiss/download_file:0.4',
         arguments=[],
         file_outputs={
-            'df_01': '/tmp/df_01.csv',
-            'df_02': '/tmp/df_02.csv',
-            'df_03': '/tmp/df_03.csv',
+            'df_customer': '/tmp/df_customer.csv',
+            'df_purchase': '/tmp/df_purchase.csv',
+            'df_product': '/tmp/df_product.csv',
+        }
+    )
+
+def preprocessData_op(df_customer, df_purchase, df_product):
+
+    return dsl.ContainerOp(
+        name='Preprocess Data',
+        image='crysiss/preprocess-data:0.7',
+        arguments=[
+            '--df_customer', df_customer,
+            '--df_purchase', df_purchase,
+            '--df_product', df_product
+        ],
+        file_outputs={
+            'df': '/tmp/df.csv'
         }
     )
 
 
-
-
-
-@dsl.pipeline(name='REC_XGBOOST Pipeline',
+@dsl.pipeline(name='XGBOOST-REC Pipeline',
               description='A pipeline that trains and logs a classification model'
 )
 def data_pipeline():
     _downloadFile_op = downloadFile_op()
 
+    _preprocessData_op = preprocessData_op(
+        dsl.InputArgumentPath(_downloadFile_op.outputs['df_customer']),
+        dsl.InputArgumentPath(_downloadFile_op.outputs['df_purchase']),
+        dsl.InputArgumentPath(_downloadFile_op.outputs['df_product'])
+    ).after(_downloadFile_op)
+
 
 if __name__ == "__main__":
     import kfp.compiler as compiler
-    compiler.Compiler().compile(data_pipeline, "data_pipeline.tar.gz")
+    compiler.Compiler().compile(data_pipeline, "data_pipeline5.tar.gz")
